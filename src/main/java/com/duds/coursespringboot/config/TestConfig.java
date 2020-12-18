f4cd84d8c15737161a044688ffa0114669436aa2
Eduardo Moraes Comerlato <eduardomc94@gmail.com>
2020-12-18 15:45:29 -0300

    Category

--- src/main/java/com/duds/coursespringboot/config/TestConfig.java
+++ src/main/java/com/duds/coursespringboot/config/TestConfig.java
@@ -1,8 +1,10 @@
 package com.duds.coursespringboot.config;
 
+import com.duds.coursespringboot.entities.Category;
 import com.duds.coursespringboot.entities.Order;
 import com.duds.coursespringboot.entities.User;
 import com.duds.coursespringboot.entities.enums.OrderStatus;
+import com.duds.coursespringboot.repositories.CategoryRepository;
 import com.duds.coursespringboot.repositories.OrderRepository;
 import com.duds.coursespringboot.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,8 +25,17 @@ public class TestConfig implements CommandLineRunner {
     @Autowired
     private OrderRepository orderRepository;
 
+    @Autowired
+    private CategoryRepository categoryRepository;
+
     @Override
     public void run(String... args) throws Exception {
+
+        Category cat1 = new Category(null, "Electronics");
+        Category cat2 = new Category(null, "Books");
+        Category cat3 = new Category(null, "Computers");
+
+        categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 
         User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
         User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
